d22876d20492a479c5164c261733e32760358b98
shabbir-shakudo <shabbir@shakudo.io>
2025-12-01 13:20:04 -0500

    fix hello_client.sh to correct client name variable and typo in termination message

--- scripts/hello_client.sh
+++ scripts/hello_client.sh
@@ -1,3 +1,3 @@
 echo "Starting the job..."
-echo "The client name is: ${process.env.CLIENT_NAME}"
-echo "Termincating the job..."
+echo "The client name is: ${CLIENT_NAME}"
+echo "Terminating the job..."
